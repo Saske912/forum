1263169137bdccb65bf4ae75b335450855d5645f
Saske912 <saveloy@yandex.ru>
2023-02-21 19:10:04 +0300

    инит

--- utils/theme.js
+++ utils/theme.js
@@ -3,7 +3,22 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
     palette: {
         primary: {
-            main: "#fcba03",
+            main: "#580b2d",
         },
+        secondary: {
+            main: "#1d1a24"
+        },
+        error: {
+            main: "#ff6652"
+        },
+        success: {
+            main: "#53e0ea"
+        },
+        info: {
+            main: "#c272d4"
+        },
+        warning: {
+            main: "#ec983c"
+        }
     },
 });
